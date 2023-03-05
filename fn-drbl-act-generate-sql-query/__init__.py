aa630c4ffbf269742a02bd1c945be8a5eea3c364
brijlnt <122353387+brijlnt@users.noreply.github.com>
2023-03-05 18:28:50 +0530

    Update __init__.py

--- fn-drbl-act-generate-sql-query/__init__.py
+++ fn-drbl-act-generate-sql-query/__init__.py
@@ -15,8 +15,34 @@ def get_prompt_text(prompt_lines, text_query):
     return ( '### Postgres SQL tables, with their properties:\\n#\\n' + schema_text + 
             '#\\n### A query to ' + text_query +'\\nSELECT' )
 
+def get_probabale_tablelist(text_query):
+    stop_words = list(get_stop_words('en'))         #About 900 stopwords
+    nltk_words = list(stopwords.words('english')) #About 150 stopwords
+    stop_words.extend(nltk_words)
+    #  word_list =  "show all sales_orders of customers"
+    tokens = word_tokenize(text_query)
+    req_table_list = [w for w in tokens if not w in stop_words]
+    return req_table_list
+
+def generate_prompt_list(data, probabale_tablelist)
+    table_list = data["table_name"]
+    join_table_list = data["join_table"]
+    final_list = []
+    for table_name,join_table in zip(table_list,join_table_list):
+        for j in range(len(req_table_list)):
+            if table_name == req_table_list[j]:   #This logic can be changed to cater to spelling mistakes
+                final_list.append(table_name)       
+                if join_table ==join_table:
+                    for kv in join_table.split(","):
+                        final_list.append(kv)
+    list_for_prompt = set(final_list)
+    filterd_data = data[data['table_name'].isin(list_for_prompt)]
+    return(filterd_data['line'].values.tolist())
+    
 def generate_openai_prompt(host, port, database, user, password, text_query):
-    sql_query = "Select line from config.prompt where include is true;"
+    #added code to creare optimized prompt
+    probabale_tablelist = get_probabale_tablelist(text_query)
+    sql_query = "Select * from config.prompt;"
     with psycopg2.connect(host=host, 
                     port=port, 
                     database=database, 
@@ -26,7 +52,8 @@ def generate_openai_prompt(host, port, database, user, password, text_query):
                 cursor.execute(sql_query)
                 df = pd.DataFrame(cursor.fetchall(), 
                             columns=[desc[0] for desc in cursor.description])
-                prompt_lines = df['line'].values.tolist()
+                prompt_lines = generate_prompt_list(df, probabale_tablelist)
+      #          df['line'].values.tolist()
     return get_prompt_text(prompt_lines, text_query)
 
 def prompt_openai(prompt):
