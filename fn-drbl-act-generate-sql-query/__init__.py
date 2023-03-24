ea2e1d83ccd6f7a2bfee28b29f673a150fb85fa5
brijlnt <122353387+brijlnt@users.noreply.github.com>
2023-03-24 16:41:24 +0530

    Update __init__.py
    Reverting again

--- fn-drbl-act-generate-sql-query/__init__.py
+++ fn-drbl-act-generate-sql-query/__init__.py
@@ -103,79 +103,60 @@ import openai
 import logging
 import pandas as pd
 import psycopg2
-from azure.storage.blob import BlobServiceClient, generate_blob_sas, BlobSasPermissions
-from llama_index import SimpleDirectoryReader, GPTListIndex, GPTSimpleVectorIndex, LLMPredictor, PromptHelper
-import os
 
-os.environ["OPENAI_API_KEY"] = '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'
+def get_prompt_text(prompt_lines, text_query):
+    schema_text=''
+    for pl in prompt_lines:
+        schema_text+= f'# {pl}\\n'
+    return ( '### Postgres SQL tables, with their properties:\\n#\\n' + schema_text + 
+            '#\\n### A query to ' + text_query +'\\nSELECT' )
 
+def generate_openai_prompt(host, port, database, user, password, text_query):
+    sql_query = "Select line from config.prompt where include is true;"
+    with psycopg2.connect(host=host, 
+                    port=port, 
+                    database=database, 
+                    user=user, 
+                    password=password) as conn:
+        with conn.cursor() as cursor:
+                cursor.execute(sql_query)
+                df = pd.DataFrame(cursor.fetchall(), 
+                            columns=[desc[0] for desc in cursor.description])
+                prompt_lines = df['line'].values.tolist()
+    return get_prompt_text(prompt_lines, text_query)
 
-# def get_prompt_text(prompt_lines, text_query):
-#     schema_text=''
-#     for pl in prompt_lines:
-#         schema_text+= f'# {pl}\\n'
-#     return ( '### Postgres SQL tables, with their properties:\\n#\\n' + schema_text + 
-#             '#\\n### A query to ' + text_query +'\\nSELECT' )
+def prompt_openai(prompt):
+    logging.info(prompt)
+    return openai.Completion.create(
+        engine="LTIM",
+        prompt=prompt,
+        temperature=0,
+        max_tokens=150,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
+        stop=["#", ";"]
+    )
 
-# def generate_openai_prompt(host, port, database, user, password, text_query):
-#     sql_query = "Select line from config.prompt where include is true;"
-#     with psycopg2.connect(host=host, 
-#                     port=port, 
-#                     database=database, 
-#                     user=user, 
-#                     password=password) as conn:
-#         with conn.cursor() as cursor:
-#                 cursor.execute(sql_query)
-#                 df = pd.DataFrame(cursor.fetchall(), 
-#                             columns=[desc[0] for desc in cursor.description])
-#                 prompt_lines = df['line'].values.tolist()
-#     return get_prompt_text(prompt_lines, text_query)
-
-def generate_openai_prompt(text_query):
-    
-    storage_account_name = 'azdatastrgl2zbtzoyzxtm6'
-    container_name = 'vector'
-  
-    blob_service_client = BlobServiceClient(
-    account_url=f"https://{storage_account_name}.blob.core.windows.net",
-                 credential='0LWc9BUPnXAuZVunZWjgY0rBoF4PPPNmWzPs5C+Yg6alKLSNegBX7XWLjsL0wKDGpcTy+7YavjMy+AStG+Utwg=='
-                )
-    
-    blob_client = blob_service_client.get_blob_client(container=container_name, blob="index.json")
-    with open(file=os.path.join('embedd.json'), mode="wb") as sample_blob:
-        download_stream = blob_client.download_blob()
-        sample_blob.write(download_stream.readall())
-    index = GPTSimpleVectorIndex.load_from_disk('embedd.json')
-    response = index.query(text_query, response_mode="compact")
-    return response.replace('\n', '')
-
-# def prompt_openai(prompt):
-#     logging.info(prompt)
-#     return openai.Completion.create(
-#         engine="LTIM",
-#         prompt=prompt,
-#         temperature=0,
-#         max_tokens=150,
-#         top_p=1,
-#         frequency_penalty=0,
-#         presence_penalty=0,
-#         stop=["#", ";"]
-#     )
-
-# def generate_sql_query(prompt_text):
-#     response = prompt_openai(prompt_text)
-#     s = response["choices"][0]["text"]
-#     s = s.replace('\\n', ' ').replace('\n', ' ')
-#     # print(response)
-#     return f'select {s};'        
+def generate_sql_query(prompt_text):
+    response = prompt_openai(prompt_text)
+    s = response["choices"][0]["text"]
+    s = s.replace('\\n', ' ').replace('\n', ' ')
+    # print(response)
+    return f'select {s};'        
       
     
 def main(params) -> str:
     try:
         prompt_text = generate_openai_prompt(
+                params['host'], 
+                params['port'], 
+                params['database'], 
+                params['user'], 
+                params['password'], 
                 params['text_query'])
 
-        return prompt_text
+        return generate_sql_query(prompt_text)    
     except Exception as e:
         logging.exception(e)
         return str(e)
